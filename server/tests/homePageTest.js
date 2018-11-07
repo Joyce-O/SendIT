484b6a32fd793445e9dc0e9e77c0e10028ff155a
obijoyce@gmail.com <obijoyce@gmail.com>
2018-11-07 21:11:43 +0100

    [ch #161789773] Add test for invalid url

--- server/tests/homePageTest.js
+++ server/tests/homePageTest.js
@@ -6,15 +6,27 @@ import app from '../../app';
 chai.use(chaiHttp);
 const { expect } = chai;
 
-
-describe('Test for Homepage API Endpoint', () => {
-  it('Should return status code 200 for success', (done) => {
-    chai.request(app)
-      .get('/api/v1')
-      .end((error, response) => {
-        expect(response).to.have.status(200);
-        expect(response.body.message).to.equal('Welcome to MailXpress, your package is safe with us');
-        done();
-      });
-  });
-});
+describe('Tests for Homepage and invalid url endpoints', () => {
+    describe('Test for Homepage API Endpoint', () => {
+        it('Should return status code 200 for success', (done) => {
+            chai.request(app)
+                .get('/api/v1')
+                .end((error, response) => {
+                    expect(response).to.have.status(200);
+                    expect(response.body.message).to.equal('Welcome to MailXpress, your package is safe with us');
+                    done();
+                });
+        });
+    });
+    describe('Test for Invalid URL', () => {
+        it('Should return status code 404 for failure', (done) => {
+            chai.request(app)
+                .get('/nonexistence/url')
+                .end((error, response) => {
+                    expect(response).to.have.status(404);
+                    expect(response.body.message).to.equal('Sorry! This page does not exist, enter a valid url.');
+                    done();
+                });
+        });
+    });
+})
