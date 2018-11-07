cf58634dc6404962e2a31ea6fcc39f899fa6eae4
obijoyce@gmail.com <obijoyce@gmail.com>
2018-11-07 15:16:13 +0100

    [ft #161779391] Add invalid url route

--- server/routes/homePageRoute.js
+++ server/routes/homePageRoute.js
@@ -2,10 +2,17 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/v1', (request, response) =>{
+router.get('/api/v1', (request, response) => {
     return response.status(200)
     .json({
         message: 'Welcome to MailXpress, your package is safe with us'
+    });
+});
+
+router.all('*', (request, response) => {
+    response.status(404)
+    .json({
+        message: 'Sorry! This page does not exist'
     });
 });
 
