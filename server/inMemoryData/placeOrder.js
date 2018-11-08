4aa1e7e90ab95b3efb532f15c5fb4d24a1df8d54
obijoyce@gmail.com <obijoyce@gmail.com>
2018-11-08 22:19:10 +0100

    [ft #161796098] Add mock input object

---
+++ server/inMemoryData/placeOrder.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id: 1,
+        senderName: "Julie John",
+        email: "jujo@hotmail.com",
+        senderPhone: "2348000008",
+        weight: "2kg",
+        parcelContent: "40 packs of gloves",
+        pickupLocation: "12 Owerri Street, Jos",
+        parcelType: "delicate",
+        receiverName: "Sopuru Bing",
+        destinationAddress: "40 Johnson Street, Lagos",
+        receiverPhone: "2348000008",
+        presentLocation: "Benin",
+        trackingID: "219b1e64-afe5-5e71-ac0b-8b6a4c605a01",
+        quantity: "3",
+        price: "400",
+        total: "1250",
+        status: "pending"
+
+    }
+];
