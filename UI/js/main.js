f733477384d015bd8b098c763e3fa654703f898e
obijoyce@gmail.com <obijoyce@gmail.com>
2018-11-16 06:58:41 +0100

    [#gh-pages] Update admin panel

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,3 +1,34 @@
+let accordion2 = document.getElementsByClassName("dashboard-accordion");
+for (i of accordion2) {
+  i.addEventListener("click",  function () {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+const toggleNav = document.querySelector("#toggleNav");
+const open = document.getElementById("open");
+
+ const switchForm = () => {
+     console.log("Clickde");
+     if (open.style.display === "none"){
+       return open.style.display = "flex";   
+    }else{
+        return open.style.display = "none";
+    }
+
+};
+
+
+toggleNav.addEventListener('click', switchForm);
+
+
+
 const signup = document.getElementById('signup');
 const is_exist = document.getElementById('exist');
 const modal = document.getElementById("modal");
