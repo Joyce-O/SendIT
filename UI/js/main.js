4a67fd3c391968027d8beb5ab3141dc1168cd499
obijoyce@gmail.com <obijoyce@gmail.com>
2018-11-16 11:49:16 +0100

    [#gh-pages] Delete unused comments

--- UI/js/main.js
+++ UI/js/main.js
@@ -29,17 +29,36 @@ toggleNav.addEventListener('click', switchForm);
 
 
 
-const signup = document.getElementById('signup');
-const is_exist = document.getElementById('exist');
-const modal = document.getElementById("modal");
-const auth_form = document.getElementById('auth-form');
+// const signup = document.getElementById('signup');
+// const is_exist = document.getElementById('exist');
+// const modal = document.getElementById("modal");
+// const auth_form = document.getElementById('auth-form');
+
+// const modalForm = (event) => {
+//     if (event.target === signup) {
+//         modal.style.display = 'block';
+//         auth_form.style.display = 'block';
+//     }
+// };
+
+// signup.addEventListener('click', modalForm);
+
+// const is_exist = document.getElementById('exist');
+const modal = document.getElementById("modal2");
+const authForm = document.getElementById('auth-form2');
+
 
 const modalForm = (event) => {
-    if (event.target === signup) {
-        modal.style.display = 'block';
-        auth_form.style.display = 'block';
-    }
+  if (event.target.id === 'signup2') {
+    modal.style.display = 'block';
+    authForm.style.display = 'block';
+
+  }
 };
-
+const signup = document.querySelector('#signup2');
 signup.addEventListener('click', modalForm);
-
+window.onclick = (event) => {
+  if (event.target === modal || event.target.id === 'close-signup') {
+    modal.style.display = 'none';
+  }
+};
